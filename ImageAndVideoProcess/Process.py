fa631ec91fba6f237b3920a863f9aefd840feade
yuxiang Liu <liuyuxiang309@gmail.com>
2022-03-07 21:46:36 -0600

    resize All image

--- ImageAndVideoProcess/Process.py
+++ ImageAndVideoProcess/Process.py
@@ -15,9 +15,22 @@ resized_img = cv2.resize(img,(int(img.shape[1]/2),int(img.shape[0]/2)))
 #write image into new file
 cv2.imwrite("galaxy_resized.jpg",resized_img)
 
-#show image
-cv2.imshow("galaxy",resized_img)
-# 0 means user press any to close the window
-# other is the time in millioseconds
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# #show image
+# cv2.imshow("galaxy",resized_img)
+# # 0 means user press any to close the window
+# # other is the time in millioseconds
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+
+import glob
+images = glob.glob("sample_images/*.jpg")
+print(images)
+for image in images:
+    # print(image)
+    imag = cv2.imread(image,0)
+    rz = cv2.resize(imag,(100,100))
+    # cv2.imshow("hey",rz)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+    cv2.imwrite("resized_"+image,rz)
